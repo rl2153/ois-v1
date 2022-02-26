de60abf982c5b2106d75c425d9090524d89fdf48
roklikar <likarr195@gmail.com>
2022-02-26 16:27:43 +0100

    program sprejme nadmorsko visino

---
+++ Gravitacija.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Gravitacija {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vnesite nadmorsko visino: ");
+        double n = sc.nextDouble();
+    }
+} 
